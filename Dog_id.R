b5147883d7e86b6a7fd78901aa9d1a31a4a7340d
Tosin Dairo <lade@Tosins-MacBook-Pro.local>
2018-12-06 18:25:38 +0000

    Second commit; adding train and test images and Rscript

--- Dog_id.R
+++ Dog_id.R
@@ -1,26 +1,19 @@
-#Dog Breeds Identifier
+#Dog Breeds
 
+
+##clear up working space
+rm(list = ls())
 
 ###load libraries
-
-```{r}
 if (!requireNamespace("BiocManager", quietly = TRUE))
-    install.packages("BiocManager")
+  install.packages("BiocManager")
 BiocManager::install("EBImage")
 
 library(EBImage)
 library(keras)
-```
-
-
-
-```{r}
-rm(list = ls())
-```
 
 
 ###read data in
-```{r}
 getwd()
 setwd("/Users/lade/Documents/Tosin_R_root/Breed/breed_pics")
 pics <- c('h1.jpg', 'h2.jpg', 'h3.jpg', 'h4.jpg', 'h5.jpg', 'h6.jpg', 'h7.jpg', 'h8.jpg', 'h9.jpg', 'r1.jpg', 'r2.jpg', 'r3.jpg', 'r4.jpg', 'r5.jpg', 'r6.jpg', 'r7.jpg', 'r8.jpg', 'r9.jpg')
@@ -28,39 +21,35 @@ mypic <- list()
 for (i in 1:18) {
   mypic[[i]] <- readImage(pics[i])
 }
-```
+
 
 
 ###explore
-```{r}
 print(mypic[[1]])
 display(mypic[[11]])
 summary(mypic[[4]])
 hist(mypic[[6]])
 str(mypic)
-```
+
 
 
 ###Resize
-```{r}
 for (i in 1:18) {
   mypic[[i]] <- resize(mypic[[i]], 28, 28)
 }
 str(mypic)
-```
+
 
 
 ###reshape
-```{r}
 for (i in 1:18) {
   mypic[[i]] <- array_reshape(mypic[[i]], c(28, 28, 3))
 }
 str(mypic)
-```
+
 
 
 ###row bind
-```{r}
 trainx <- NULL
 for (i in 1:7) {
   trainx <- rbind(trainx, mypic[[i]])
@@ -74,18 +63,16 @@ for (i in 8:9) {
 str(testx)
 trainy <- c(0,0,0,0,0,0,0,1,1,1,1,1,1,1)
 testy <- c(0,0,1,1)
-```
+
 
 
 ###one hot encoding
-```{r}
 trainLabels <- to_categorical(trainy)
 testLabels <- to_categorical(testy)
-```
+
 
 
 ###create model
-```{r}
 model <- keras_model_sequential()
 model %>%
   layer_dense(units = 256, activation = 'relu', input_shape = c(2352)) %>%
@@ -93,20 +80,18 @@ model %>%
   layer_dense(units = 2, activation = 'softmax')
 
 summary(model)
-```
+
 
 
 ###compile
-```{r}
 model %>% 
   compile(loss = 'binary_crossentropy',
           optimizer = optimizer_rmsprop(),
           metrics = c('accuracy'))
-```
+
 
 
 ###fit model
-```{r}
 history <- model %>%
   fit(trainx,
       trainLabels,
@@ -114,46 +99,18 @@ history <- model %>%
       batch_size = 32,
       validation_split = 0.2)
 plot(history)
-```
 
 
 ###evaluation and prediction of train data
-```{r}
 model %>% evaluate(trainx, trainLabels)
 pred <- model %>% predict_classes(trainx)
 table(Predicted = pred, Actual = trainy)
 prob <- model %>% predict_proba(trainx)
 cbind(prob, Predicted = pred, Actual = trainy)
-```
+
 
 
 ###evaluation and prediction of test data
-```{r}
 model %>% evaluate(testx, testLabels)
 pred_test <- model %>% predict_classes(testx)
 table(Predicted = pred_test, Actual = testy)
-```
-
-
-
-```{r}
-
-```
-
-
-
-```{r}
-
-```
-
-
-
-```{r}
-
-```
-
-
-
-```{r}
-
-```
